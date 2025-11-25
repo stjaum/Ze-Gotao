e2752cd46ea960f7cb9e9d6b265ee19dc8a277a7
JOAO VICTOR VITURINO DA SILVA <superbag15@gmail.com>
2025-11-25 20:03:27 -0300

    gsdfg

--- ZeGotao/Controllers/HomeController.cs
+++ ZeGotao/Controllers/HomeController.cs
@@ -1,42 +1,100 @@
-using System.Diagnostics;
+using System;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ZeGotao.Data;
 using ZeGotao.Models;
 
-namespace WebApplication1.Controllers
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ZeGotaoContext _db;
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ZeGotaoContext db, ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
+        _db = db;
+        _logger = logger;
+    }
 
-        public HomeController(ILogger<HomeController> logger)
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    public IActionResult Entrar()
+    {
+        return View(new Usuario());
+    }
+    public IActionResult Cadastrar()
+    {
+        return View(new Usuario());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Entrar(Usuario usuario)
+    {
+        if (!ModelState.IsValid)
+            return View(usuario);
+
+        var user = await _db.Usuario
+            .FirstOrDefaultAsync(x => x.Email == usuario.Email && x.Senha == usuario.Senha);
+
+        if (user == null)
         {
-            _logger = logger;
+            ViewData["ErroLogin"] = "E-mail ou senha inválidos!";
+            return View(usuario);
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+        HttpContext.Session.SetInt32("IdUsuario", user.IdUsuario);
+        HttpContext.Session.SetString("NomeUsuario", user.Nome);
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        public IActionResult Entrar()
-        {
-            return View();
-        }
-        public IActionResult Cadastrar()
-        {
-            return View();
-        }
+        return RedirectToAction("PosLogin");
+    }
 
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+
+    [HttpPost]
+    public async Task<IActionResult> Cadastrar(Usuario usuario)
+    {
+        if (!ModelState.IsValid)
+            return View(usuario);
+
+        bool existe = await _db.Usuario.AnyAsync(x => x.Email == usuario.Email);
+
+        if (existe)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            ViewData["ErroCadastro"] = "E-mail já cadastrado!";
+            return View(usuario);
         }
+
+        usuario.Ativo = true;
+        usuario.TipoUsuarioId = 1;
+
+        _db.Usuario.Add(usuario);
+        await _db.SaveChangesAsync();
+
+        return RedirectToAction("Entrar");
+    }
+
+    public IActionResult PosLogin()
+    {
+        int? id = HttpContext.Session.GetInt32("IdUsuario");
+
+        if (id == null)
+            return RedirectToAction("Entrar");
+
+        ViewBag.Nome = HttpContext.Session.GetString("NomeUsuario");
+
+        return View();
+    }
+
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+        return RedirectToAction("Entrar");
     }
 }
