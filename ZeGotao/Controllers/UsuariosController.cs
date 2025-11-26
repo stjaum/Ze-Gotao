595938a90e82d2277c3b93f272f054c02f2fbbcb
JOAO VICTOR VITURINO DA SILVA <superbag15@gmail.com>
2025-11-25 21:56:24 -0300

    zé gotão image

--- ZeGotao/Controllers/UsuariosController.cs
+++ ZeGotao/Controllers/UsuariosController.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using ZeGotao.Data;
+using ZeGotao.Data;     // ajuste namespace se diferente
 using ZeGotao.Models;
+using Microsoft.AspNetCore.Http;
+using System;
 
 namespace ZeGotao.Controllers
 {
@@ -18,179 +15,66 @@ namespace ZeGotao.Controllers
         {
             _context = context;
         }
+
+        // GET: Usuarios/Entrar
+        [HttpGet]
+        public IActionResult Entrar()
+        {
+            return View();
+        }
+
+        // POST: Usuarios/EntrarPost
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> EntrarPost(string email, string senha)
         {
-            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(senha))
+            if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(senha))
             {
-                ViewData["Erro"] = "Preencha todos os campos.";
+                ViewData["Erro"] = "Preencha email e senha.";
                 return View("Entrar");
             }
 
-            // Busca no banco
-            var user = await _context.Usuario
+            // Ajuste para o DbSet/Model do seu projeto: pode ser _context.Usuario ou _context.Usuarios
+            var user = await _context.Usuario // OU _context.Usuarios conforme seu AppDbContext
                 .FirstOrDefaultAsync(u => u.Email == email && u.Senha == senha);
 
             if (user == null)
             {
-                ViewData["Erro"] = "Email ou senha inválidos!";
+                ViewData["Erro"] = "E-mail ou senha inválidos!";
                 return View("Entrar");
             }
 
-            // Guarda o ID logado temporariamente
-            HttpContext.Session.SetInt32("UserId", user.IdUsuario);
+            // Salva dados na sessão (precisa de app.UseSession())
+            HttpContext.Session.SetInt32("IdUsuario", user.IdUsuario); // ajuste propriedade do Id se necessário
+            HttpContext.Session.SetString("NomeUsuario", user.Nome);
 
             return RedirectToAction("PosLogin", "Usuarios");
         }
-        public IActionResult Cadastrar()
-        {
-            return View(new Usuario());
-        }
-        public IActionResult Entrar()
-        {
-            return View(new Usuario());
-        }
 
-        // GET: Usuarios
-        public async Task<IActionResult> Index()
+        // GET: Usuarios/PosLogin
+        [HttpGet]
+        public async Task<IActionResult> PosLogin()
         {
-            var zeGotaoContext = _context.Usuario.Include(u => u.TipoUsuario);
-            return View(await zeGotaoContext.ToListAsync());
-        }
-
-        // GET: Usuarios/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
+            var id = HttpContext.Session.GetInt32("IdUsuario");
             if (id == null)
+                return RedirectToAction("Entrar");
+
+            // buscar novamente para ter dados atualizados
+            var user = await _context.Usuario.FindAsync(id.Value);
+            if (user == null)
             {
-                return NotFound();
+                HttpContext.Session.Clear();
+                return RedirectToAction("Entrar");
             }
 
-            var usuario = await _context.Usuario
-                .Include(u => u.TipoUsuario)
-                .FirstOrDefaultAsync(m => m.IdUsuario == id);
-            if (usuario == null)
-            {
-                return NotFound();
-            }
-
-            return View(usuario);
+            return View(user);
         }
 
-        // GET: Usuarios/Create
-        public IActionResult Create()
+        // Opcional: Logout
+        public IActionResult Logout()
         {
-            ViewData["TipoUsuarioId"] = new SelectList(_context.TipoUsuario, "IdTipoUsuario", "DescricaoTipoUsuario");
-            return View();
-        }
-
-        // POST: Usuarios/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("IdUsuario,Nome,Email,Senha,Cpf,Telefone,Endereco,DataNascimento,TipoUsuarioId,Ativo")] Usuario usuario)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(usuario);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["TipoUsuarioId"] = new SelectList(_context.TipoUsuario, "IdTipoUsuario", "DescricaoTipoUsuario", usuario.TipoUsuarioId);
-            return View(usuario);
-        }
-
-        // GET: Usuarios/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var usuario = await _context.Usuario.FindAsync(id);
-            if (usuario == null)
-            {
-                return NotFound();
-            }
-            ViewData["TipoUsuarioId"] = new SelectList(_context.TipoUsuario, "IdTipoUsuario", "DescricaoTipoUsuario", usuario.TipoUsuarioId);
-            return View(usuario);
-        }
-
-        // POST: Usuarios/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("IdUsuario,Nome,Email,Senha,Cpf,Telefone,Endereco,DataNascimento,TipoUsuarioId,Ativo")] Usuario usuario)
-        {
-            if (id != usuario.IdUsuario)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(usuario);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!UsuarioExists(usuario.IdUsuario))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["TipoUsuarioId"] = new SelectList(_context.TipoUsuario, "IdTipoUsuario", "DescricaoTipoUsuario", usuario.TipoUsuarioId);
-            return View(usuario);
-        }
-
-        // GET: Usuarios/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var usuario = await _context.Usuario
-                .Include(u => u.TipoUsuario)
-                .FirstOrDefaultAsync(m => m.IdUsuario == id);
-            if (usuario == null)
-            {
-                return NotFound();
-            }
-
-            return View(usuario);
-        }
-
-        // POST: Usuarios/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var usuario = await _context.Usuario.FindAsync(id);
-            if (usuario != null)
-            {
-                _context.Usuario.Remove(usuario);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool UsuarioExists(int id)
-        {
-            return _context.Usuario.Any(e => e.IdUsuario == id);
+            HttpContext.Session.Clear();
+            return RedirectToAction("Entrar");
         }
     }
 }
